e4d3b2e27d5015f1c8ed4e6e7b4db7136ef2d171
Jaymala Sinha <jsinha@chef.io>
2019-09-13 14:28:53 -0400

    Add path for file command
    Signed-off-by: Jaymala Sinha <jsinha@chef.io>

--- scripts/bk_tests/bk_win_integration.ps1
+++ scripts/bk_tests/bk_win_integration.ps1
@@ -1,6 +1,10 @@
 echo "--- system details"
 $Properties = 'Caption', 'CSName', 'Version', 'BuildType', 'OSArchitecture'
 Get-CimInstance Win32_OperatingSystem | Select-Object $Properties | Format-Table -AutoSize
+
+# Set-Item -Path Env:Path -Value ($Env:Path + ";C:\Program Files\Git\mingw64\bin;C:\Program Files\Git\usr\bin") 
+$Env:path="C:\Program Files\Git\mingw64\bin;C:\Program Files\Git\usr\bin;C:\ruby26\bin;C:\ci-studio-common\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\ProgramData\chocolatey\bin;C:\Program Files\Git\cmd;C:\Users\ContainerAdministrator\AppData\Local\Microsoft\WindowsApps;C:\Go\bin;C:\Users\ContainerAdministrator\go\bin"
+
 ruby -v
 bundle --version
 
